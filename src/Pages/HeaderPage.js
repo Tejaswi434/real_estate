6b115275ff8252c9ebbb57e8590ef93eb9e7d543
srinivasChalamalasetti <srinivaschalamalasetti20@gmail.com>
2024-08-26 17:00:34 +0530

    initail commit

--- src/Pages/HeaderPage.js
+++ src/Pages/HeaderPage.js
@@ -1,39 +1,39 @@
-import React from 'react'
+import React from "react";
 
 function HeaderPage() {
   return (
     <div>
-        <Header
+      <Header
         style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          padding: '0 24px',
-          position: 'fixed',
-          width: '100%',
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: "0 24px",
+          position: "fixed",
+          width: "100%",
           top: 0,
           left: 0,
           zIndex: 1000,
-          height: '64px',
-          theme: "dark"
+          height: "64px",
+          theme: "dark",
         }}
       >
-        <div className='heading'>Real Estate Management System</div>
+        <div className="heading">Real Estate Management System</div>
         <Menu
           theme="dark"
           mode="horizontal"
           items={items(navigate)}
           style={{
-            fontSize: '20px',
+            fontSize: "20px",
             flex: 1,
-            justifyContent: 'flex-end',
-            border: 'none',
-            color: '#fff'
+            justifyContent: "flex-end",
+            border: "none",
+            color: "#fff",
           }}
         />
       </Header>
     </div>
-  )
+  );
 }
 
-export default HeaderPage
+export default HeaderPage;
